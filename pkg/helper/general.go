1a2441c95cb102073dae74090e02ed5471f297a5
Fahmi Amrullah <3276111905960001>
2023-08-10 16:00:12 +0700

    in array like php

--- pkg/helper/general.go
+++ pkg/helper/general.go
@@ -9,3 +9,13 @@ func PaginationLimit() int {
 	pagination, _ := strconv.Atoi(os.Getenv("pagination"))
 	return pagination
 }
+
+func InArray(slice []string, value string) int {
+	for i, v := range slice {
+		if v == value {
+			return i
+		}
+	}
+	return -1
+}
+
