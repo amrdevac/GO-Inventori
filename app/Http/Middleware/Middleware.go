da227182a8781522940956ebd0e1acc7950d6b4a
Fahmi Amrullah <3276111905960001>
2023-08-20 22:44:31 +0700

    enhance middleware

--- app/Http/Middleware/Middleware.go
+++ app/Http/Middleware/Middleware.go
@@ -2,32 +2,46 @@ package Middleware
 
 import (
 	"inventori/app/Provider/ResponseHandler"
+	"inventori/pkg/helper"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 )
 
 func VerifyJWT(c *gin.Context) {
-	header := c.Request.Header["Token"]
+	header := c.Request.Header["Authorization"]
+	if !helper.CompareChecker(strings.Join(header, ""), "==", "", "Authorization required") {
+		c.Abort()
+	}
+
+	hasil := strings.TrimSpace(strings.Replace(header[0], "Bearer", "", -1))
 
 	if header == nil {
 		ResponseHandler.Go(c).
 			SetMessage("Unauthorize").
 			SetHttpStatus(http.StatusBadRequest).
-			Apply().StopProcess()
+			Apply()
+		c.Abort()
+
 	}
 
 	claims := &jwt.StandardClaims{}
-	_, err := jwt.ParseWithClaims(header[0], claims,
+	_, err := jwt.ParseWithClaims(hasil, claims,
 		func(token *jwt.Token) (interface{}, error) {
 			return []byte("secret"), nil
 		})
-		
+
 	if err != nil {
 		ResponseHandler.Go(c).
 			SetMessage("Access Unauthorized").
 			SetHttpStatus(http.StatusUnauthorized).
-			Apply().StopProcess()
+			Apply()
+		c.Abort()
+
 	}
+
+	c.Next()
+
 }
